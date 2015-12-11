19c3388e37caba7dd201f5066916c2dfeb1092f5
James Walsh <walshj19@tcd.ie>
2015-12-11 19:33:38 +0000

    Initial html and js starter files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function setupCanvas(){
+	// setup for the canvas
+	var canvas = document.getElementById('viewport');
+	var ctx = canvas.getContext('2d');
+}
+
+var xpos = 50;
+var ypos = 50;
+
+console.log("start");
